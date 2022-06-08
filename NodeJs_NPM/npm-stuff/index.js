eb7bbe2e84a2fd8984aa53ee1a558b7993a06b30
pezantish <e.e.a.sadek@gmail.com>
2022-06-08 22:54:52 +0100

    updated express, teething issues

--- NodeJs_NPM/npm-stuff/index.js
+++ NodeJs_NPM/npm-stuff/index.js
@@ -1,28 +1,40 @@
-const http = require('http');
+// const http = require('http');
 const morgan = require('morgan');
+const express = require('express');
 
-const host = 'localhost';
+//const host = 'localhost';
 const port = 3000;
+const app = express();
 
-const logger = morgan('dev');
+app.use(morgan('dev'));
 
-const server = http.createServer((req, res) => {
-    logger(req, res, err => {
-        
-        res.setHeader('Content-Type', 'text/plain');
-        if (err) {
-            console.log(err);
-            res.statusCode = 500;
-            res.end('Something went wrong');
-            return;
-        }
-        res.statusCode = 200;
-        res.end('Hello');
-    });
+//TASK 2: ERROR
+public = __dirname + '/public';
+app.use(express.static(public));
+
+app.get('/', (req, res) => {
+    res.send('Greetings');
 });
 
-server.listen(port, host, () => {
-    console.log(`Server up on ${host}:${port}`);
+//TASK 1
+app.get('/hello', (req, res) => {
+    res.send('Hello');
+});
+
+app.get('/world', (req, res) => {
+    res.send('World');
+});
+//END
+
+//TASK 3: UNFINISHED
+app.post('/universe', (req, res) => {
+    res.send('World');
+});
+
+
+const server = app.listen(port, () => {
+    //server.address().address is host
+    console.log(`Server up on ${server.address().address}:${port}`);
 });
 
 console.log("It's ALIVE!");
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# nodejs
+# Wednesday Tasks
+The majority of the day was spent on understanding theoretically, as opposed to practical code. This will be updated later to fully evidence my competency.
